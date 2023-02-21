805b64141202b4b5bb8e2ecbd417d367fed51e80
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-21 16:24:54 -0600

    Updated game.py file

--- game.py
+++ game.py
@@ -1,6 +1,18 @@
 greeting = """Welcome to Rock Paper Scissors Lizard Spock!
 
 Best out of three matches wins!
+
+Rules for game:
+Scissors cut Paper
+Paper covers Rock
+Rock crushes Scissors
+Rock crushes Lizard
+Lizard poisons Spock
+Spock smashes Scissors
+Scissors decapitates Lizard
+Lizard eats Paper
+Paper disproves Spock
+Spock vaporizes Rock
 """
 
 print(greeting)
