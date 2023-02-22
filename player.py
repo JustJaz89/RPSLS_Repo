734ed10329926296e9078497293c0f2bcd38acd8
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-22 10:08:55 -0600

    Updated player.py file

--- player.py
+++ player.py
@@ -1,4 +1,48 @@
+import random
+
 class Player:
-    def __init__(self):
-        self.name = ""
-        pass
+    def __init__(self, name):
+        self.name = name
+
+    def number_to_name(number):
+        if number == 0:
+            return "Rock"
+        elif number == 1:
+            return "Spock"
+        elif number == 2:
+            return "Paper"
+        elif number == 3:
+            return "Lizard"
+        elif number == 4:
+            return "Scissors"
+        else:
+            return "Enter a valid number, 0-4: "
+
+    def rpsls(player_choice):
+        print("")
+        print("Player chooses " + player_choice)
+        player_number = number_to_name(player_choice)
+        computer_number = random.randrange(0, 5)
+        computer_choice = number_to_name(computer_number)
+        print("Computer chooses " + computer_choice)
+
+        if computer_number - player_number > 0:
+            if computer_number - player_number > 2:
+                print("Player wins!")
+            elif computer_number - player_number <= 2:
+                print("Computer wins!")
+        elif computer_number - player_number < 0:
+            if (computer_number - player_number) %5 > 2:
+                print("Player wins!")
+            elif computer_number - player_number <= 2:
+                print("Computer wins!")
+        elif computer_number == player_number:
+            print("Player and computer tie!")
+        else:
+            print("Invalid input.")
+
+# rpsls(0)
+# rpsls(1)
+# rpsls(2)
+# rpsls(3)
+# rpsls(4)
