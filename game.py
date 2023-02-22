d3e2d37d322df581d93abb67996ee5081f687bd2
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-22 10:11:42 -0600

    Updated game.py file

--- game.py
+++ game.py
@@ -1,42 +1,76 @@
-greeting = """Welcome to Rock Paper Scissors Lizard Spock!
+import player
+import human
+import ai
 
-Best out of three matches wins!
+class Game:
+    def __init__(self) -> None:
+        pass
 
-Rules for game:
-Scissors cut Paper
-Paper covers Rock
-Rock crushes Scissors
-Rock crushes Lizard
-Lizard poisons Spock
-Spock smashes Scissors
-Scissors decapitates Lizard
-Lizard eats Paper
-Paper disproves Spock
-Spock vaporizes Rock
-"""
+    greeting = """Welcome to Rock Paper Scissors Lizard Spock!
 
-print(greeting)
+    Best out of three matches wins!
 
-input = ("How many players? 1 or 2?")
+    Rules for game:
+    Scissors cut Paper
+    Paper covers Rock
+    Rock crushes Scissors
+    Rock crushes Lizard
+    Lizard poisons Spock
+    Spock smashes Scissors
+    Scissors decapitates Lizard
+    Lizard eats Paper
+    Paper disproves Spock
+    Spock vaporizes Rock
+    """
 
-input = ("""Enter a number for your selection: 
-1. Rock
-2. Paper
-3. Scissors
-4. Lizard
-5. Spock
-""")
+    print(greeting)
 
-def number_to_name(number):
-    if number == 1:
-        return "Rock"
-    elif number == 2:
-        return "Paper"
-    elif number == 3:
-        return "Scissors"
-    elif number == 4:
-        return "Lizard"
-    elif number == 5:
-        return "Spock"
+    # input = ("How many players? 1 or 2?")
+
+    choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+
+    choices = input(f"""Player, please enter a number for your selection: 
+    0. Rock
+    1. Spock
+    2. Paper
+    3. Lizard
+    4. Scissors
+    """)
+
+# def number_to_name(number):
+#     if number == 0:
+#         return "Rock"
+#     elif number == 1:
+#         return "Spock"
+#     elif number == 2:
+#         return "Paper"
+#     elif number == 3:
+#         return "Lizard"
+#     elif number == 4:
+#         return "Scissors"
+#     else:
+#         return "Enter a valid number, 0-4: "
+
+def rpsls(name):
+    import random
+
+    player_number = number_to_name(name)
+    computer_number = random.randrange(5)
+    difference = (player_number - computer_number) & 5
+
+    if difference == 0:
+        winner = "It's a tie!"
+    elif difference < 3:
+        winner = "Player wins!"
     else:
-        return "Error"
+        winner = "Computer wins!"
+
+    computer_name = number_to_name(computer_number)
+
+    print("")
+    print("Player chooses " + name + ".")
+    print("Computer chooses " + computer_name + ".")
+    print(winner)
+
+def run_game():
+    pass
