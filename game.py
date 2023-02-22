e4bdbbdf940b2fa76ef215519da085b4f5b81436
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-22 17:50:28 -0600

    Updated game.py file

--- game.py
+++ game.py
@@ -36,70 +36,71 @@ class Game:
 
 input = ("How many players? 1 or 2?")
 
+def run_game():
+    if (Human == Computer):
+        print("It's a tie!")
+
+    elif Human == 'Rock':
+        if Computer == 'Scissors':
+            print('Player wins! Rock breaks Scissors')
+        else:
+            print('Computer wins!')
+
+    elif Human == 'Paper':
+        if Computer == 'Rock':
+            print('Computer wins! Paper covers Rock!')
+        else: 
+            print('Player wins!')
+
+    elif Human == 'Scissors':
+        if Computer == 'Paper':
+            print('Player wins! Scissors cut Paper!')
+        else:
+            print('Computer wins!')
+        
+    elif Human == 'Rock':
+        if Computer == 'Lizard':
+            print('Player wins! Rock crushes Lizard')
+        else:
+            print('Player wins!')
+
+    elif Human == 'Spock':
+        if Computer == 'Scissors':
+            print('Player wins! Spock smashes Scissors')
+        else:
+            print('Computer wins!')
+
+    elif Human == 'Lizard':
+        if Computer == 'Scissors':
+            print('Computer wins! Scissors decapitates Lizard!')
+        else:
+            print('Player wins!')
+
+    elif Human == 'Paper':
+        if Computer =='Lizard':
+            print('Computer wins! Lizard eats Paper!')
+        else:
+            print('Player wins!') 
+
+    elif Human == 'Spock':
+        if Computer == 'Paper':
+            print('Computer wins! Paper disproves Spock')
+        else:
+            print('Player wins!')
+
+    elif Human == 'Spock':
+        if Computer == 'Rock':
+            print('Player wins! Spock Vaporizes Rock')
+        else:
+            print('Computer wins!') 
+
 #     print("")
 #     print("Player chooses " + name + ".")
 #     print("Computer chooses " + computer_name + ".")
 #     print(winner)
 
-if (Human == Computer):
-    print("It's a tie!")
-
-elif Human == 'Rock':
-    if Computer == 'Scissors':
-       print('Player wins! Rock breaks Scissors')
-    else:
-       print('Computer wins!')
-
-elif Human == 'Paper':
-   if Computer == 'Rock':
-       print('Computer wins! Paper covers Rock!')
-   else: 
-       print('Player wins!')
-
-elif Human == 'Scissors':
-   if Computer == 'Paper':
-      print('Player wins! Scissors cut Paper!')
-   else:
-      print('Computer wins!')
-    
-elif Human == 'Rock':
-    if Computer == 'Lizard':
-       print('Player wins! Rock crushes Lizard')
-    else:
-       print('Player wins!')
-
-elif Human == 'Spock':
-    if Computer == 'Scissors':
-       print('Player wins! Spock smashes Scissors')
-    else:
-        print('Computer wins!')
-
-elif Human == 'Lizard':
-    if Computer == 'Scissors':
-       print('Computer wins! Scissors decapitates Lizard!')
-    else:
-        print('Player wins!')
-
-elif Human == 'Paper':
-    if Computer =='Lizard':
-        print('Computer wins! Lizard eats Paper!')
-    else:
-        print('Player wins!') 
-
-elif Human == 'Spock':
-    if Computer == 'Paper':
-        print('Computer wins! Paper disproves Spock')
-    else:
-        print('Player wins!')
-
-elif Human == 'Spock':
-    if Computer == 'Rock':
-        print('Player wins! Spock Vaporizes Rock')
-    else:
-        print('Computer wins!') 
-
-def run_game(self):
-    pass
+def run_game():
+    run_game
 
 
 # else:
