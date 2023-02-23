5360100bbd793463729edda446cc81168c1d7a02
kendra jarmon <kendra65239@hotmail.com>
2023-02-22 23:20:36 -0600

    updated game file

--- player.py
+++ player.py
@@ -9,20 +9,20 @@ class Player():
     def choices(self):      
         Player_is_running = True
         while Player_is_running == True:
-            player_choices = input('Please enter number for Rock(0), Paper(1), Scissors(2), Lizard(3), Spock(4): ')
-            if player_choices == '0':
+            player_choice = input('Please enter number for Rock(0), Paper(1), Scissors(2), Lizard(3), Spock(4): ')
+            if player_choice == '0':
                 self.selected_gesture = 'Rock'
                 break
-            elif player_choices == '1':
+            elif player_choice == '1':
                 self.selected_gesture = 'Paper'
                 break
-            elif player_choices == '2':
+            elif player_choice == '2':
                 self.selected_gesture = 'Scissors'
                 break
-            elif player_choices == '3':
+            elif player_choice == '3':
                 self.selected_gesture = 'Lizard'
                 break
-            elif player_choices == '4':
+            elif player_choice == '4':
                 self.selected_gesture = 'Spock'
                 break
                 #player_two = random.randint(0,4)
